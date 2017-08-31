6c115ff0c7a23da357eab0ecd39f0bdc9b0cec1e
Pragya <pragya.singh.16cse@bml.edu.in>
2017-08-31 19:06:59 +0530

    Input Java code

---
+++ Pragya/input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class input{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		System.out.println("The number is "+n);
+	}
+}
