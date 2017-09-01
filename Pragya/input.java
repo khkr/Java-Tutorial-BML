4b559e226720209234562419e1800fc4288762f7
Pragya <pragya.singh.16cse@bml.edu.in>
2017-09-02 00:19:34 +0530

    yup

--- Pragya/input.java
+++ Pragya/input.java
@@ -1,8 +1,43 @@
 import java.util.Scanner;
-public class input{
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		System.out.println("The number is "+n);
-	}
+
+public class input
+{
+public static void main(String[] args) 
+{
+
+Scanner in = new Scanner(System.in);
+Fibonacci f=new Fibonacci(5);
+f.getseries();
+
+
+}
+}
+
+class Fibonacci
+{
+int n;
+Fibonacci(int a)
+{
+n = a;
+}
+void getSeries(int number)
+{
+int a = 0;
+int b = 1;
+int c,i;
+if(n==2)
+System.out.print(a+" "+b);
+else if(n==1)
+System.out.print(a);
+else
+	{for(i=2;i<=n;i++)
+{
+	c=a+b;
+	a=b;
+	b=c;
+	System.out.print(" "+c);
+}}
+
+
+}
 }
